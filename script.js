85d16a8f4a324b9c58759bc0c8213d9dd5ee09b0
Madaka1102 <vasanthveera827@gmail.com>
2025-02-08 15:50:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const file=document.getElementById('file');
-const image=document.getElementById('image');
-function changeProfile(){
-    image.src=URL.createObjectURL(file.files[0]);
-}
-image.style.width='200px';
-
-
